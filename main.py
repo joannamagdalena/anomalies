a53d651a787edfc11cb3067b0d054c980a7413e9
Joanna <joannakaczmarek93@gmail.com>
2024-09-26 19:04:52 +0200

    mixed

--- main.py
+++ main.py
@@ -77,6 +77,7 @@ model_IF.fit(X_train, y_train)
 
 validation_IF = model_IF.predict(X_valid)
 validation_IF[validation_IF == -1] = 0
+print(type(validation_IF))
 cm_valid_IF = confusion_matrix(y_valid, validation_IF)
 print(cm_valid_IF)
 print("% of corrected predictions: ", (cm_valid_IF[0, 0]+cm_valid_IF[1, 1])/np.matrix(cm_valid_IF).sum())
@@ -102,3 +103,20 @@ validation_LOF[validation_LOF == -1] = 0
 cm_valid_LOF = confusion_matrix(y_valid, validation_LOF)
 print(cm_valid_LOF)
 print("% of corrected predictions: ", (cm_valid_LOF[0, 0]+cm_valid_LOF[1, 1])/np.matrix(cm_valid_LOF).sum())
+
+
+### mixed
+
+validation_mixed = []
+for i in range(0, len(validation_IF)):
+    if validation_IF[i] == 0 and validation_LOF[i] == 0:
+        validation_mixed.append(0)
+    elif validation_IF[i] == 1 and validation_kmeans[i] == 1:
+        validation_mixed.append(1)
+    else:
+        validation_mixed.append(validation_IF[i])
+
+validation_mixed = np.array(validation_mixed)
+cm_valid_mixed = confusion_matrix(y_valid, validation_mixed)
+print(cm_valid_mixed)
+print("% of corrected predictions: ", (cm_valid_mixed[0, 0]+cm_valid_mixed[1, 1])/np.matrix(cm_valid_mixed).sum())
