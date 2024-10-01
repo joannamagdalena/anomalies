a843bb08edc994979f6f68d91a138704ce095e17
Joanna <joannakaczmarek93@gmail.com>
2024-10-01 18:47:40 +0200

    features analysis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # mrwellsdavid/unsw-nb15
+# https://stackoverflow.com/questions/46498455/categorical-features-correlation/46498792#46498792
 import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline
@@ -10,6 +11,7 @@ from sklearn.ensemble import IsolationForest
 from sklearn.metrics import confusion_matrix
 from sklearn.cluster import KMeans
 from sklearn.neighbors import LocalOutlierFactor
+import scipy.stats as ss
 
 
 def choose_numerical_features(ds, possible_features):
@@ -20,8 +22,24 @@ def choose_numerical_features(ds, possible_features):
             correlated_features.append(feature)
     return correlated_features
 
-def choose_categorical_features(ds, possible_features):
+def cramers_v(confusion_matrix):
+    """ calculate Cramers V statistic for categorial-categorial association.
+        uses correction from Bergsma and Wicher,
+        Journal of the Korean Statistical Society 42 (2013): 323-328
+    """
+    chi2 = ss.chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum()
+    phi2 = chi2 / n
+    r, k = confusion_matrix.shape
+    phi2corr = max(0, phi2 - ((k-1)*(r-1))/(n-1))
+    rcorr = r - ((r-1)**2)/(n-1)
+    kcorr = k - ((k-1)**2)/(n-1)
+    return np.sqrt(phi2corr / min((kcorr-1), (rcorr-1)))
 
+def choose_categorical_features(ds, possible_features):
+    correlated_features = []
+    x = list(ds["label"])
+    return True
 
 def data_preprocessing(ds_train, ds_test):
     # numerical columns
