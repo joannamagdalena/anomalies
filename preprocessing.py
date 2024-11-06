228b7f15eff8391bab5bd20bd4ad26c8896f696c
Joanna <joannakaczmarek93@gmail.com>
2024-11-06 18:27:11 +0100

    division of the dataset moved

--- preprocessing.py
+++ preprocessing.py
@@ -75,6 +75,10 @@ def choose_categorical_features(ds, possible_features):
 
 
 def data_preprocessing(ds_train, ds_test):
+
+    # dividing into training and validation datasets
+    ds_train, ds_valid = train_test_split(ds_train, train_size=0.8, test_size=0.2, random_state=0)
+
     # numerical columns
     num_cols = [col for col in ds_train.columns if ds_train[col].dtype in ["int64", "float64"]
                 and col not in ["label", "sport", "dsport", "is_sm_ips_ports", "is_ftp_login", "swin", "dwin"]]
@@ -88,10 +92,16 @@ def data_preprocessing(ds_train, ds_test):
     cat_features_for_training = choose_categorical_features(ds_train, cat_cols)
 
     # removing categorical columns with too many unique values; dividing datasets
-    X_train_full = ds_train[num_features_for_training + cat_features_for_training].copy()
+    X_train = ds_train[num_features_for_training + cat_features_for_training].copy()
+    X_valid = ds_valid[num_features_for_training + cat_features_for_training].copy()
     X_test = ds_test[num_features_for_training + cat_features_for_training].copy()
-    y_train_full = pd.DataFrame(ds_train["label"].copy())
+    y_train = pd.DataFrame(ds_train["label"].copy())
+    y_valid = pd.DataFrame(ds_valid["label"].copy())
     y_test = pd.DataFrame(ds_test["label"].copy())
+
+    # additional full dataset: train + valid
+    X_train_full = pd.concat([X_train,X_valid])
+    y_train_full = pd.concat([y_train,y_valid])
 
     # regularization
     for num in num_features_for_training:
@@ -99,9 +109,6 @@ def data_preprocessing(ds_train, ds_test):
         X_train_full[num] = X_train_full[num] / m
         X_test[num] = X_test[num] / m
 
-    # dividing into training and validation datasets
-    X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full,
-                                                              train_size=0.8, test_size=0.2, random_state=0)
 
     num_transformer = SimpleImputer(strategy="most_frequent")
     cat_transformer = Pipeline(steps=[("imputer", SimpleImputer(strategy="most_frequent",)),
