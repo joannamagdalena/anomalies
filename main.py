bfb82d615795dbe04b502d46eeded0d16c4c15eb
Joanna <joannakaczmarek93@gmail.com>
2024-11-17 16:36:28 +0100

    reorganized

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# mrwellsdavid/unsw-nb15
-import eli5
-import pandas as pd
+import loading_data
 import numpy as np
 from sklearn.ensemble import IsolationForest
 from sklearn.metrics import confusion_matrix, mean_absolute_error
@@ -14,24 +12,9 @@ from xgboost import XGBRegressor
 #import eli5
 #from eli5.sklearn import PermutationImportance
 from sklearn.inspection import permutation_importance
-import oracledb
 
 
-# loading data from csv
-dataset_train_full = pd.read_csv("../unsw-nb15/UNSW_NB15_training-set.csv")
-dataset_test_full = pd.read_csv("../unsw-nb15/UNSW_NB15_testing-set.csv")
-
-dataset_train = dataset_train_full.drop(["attack_cat", "id"], axis=1)
-dataset_test = dataset_test_full.drop(["attack_cat", "id"], axis=1)
-
-# loading data from Oracle db
-connection_to_db = oracledb.connect(user="SYSTEM", password="12345", host="localhost", port=1521)
-cursor_db = connection_to_db.cursor()
-cursor_db.execute("select * from UNSW_NB15")
-dataset_train_from_oracle = pd.DataFrame(cursor_db.fetchall())
-dataset_train_from_oracle.columns = [d[0] for d in cursor_db.description]
-cursor_db.close()
-connection_to_db.close()
+dataset_train, dataset_test = loading_data.loading_data_from_csv()
 
 type_change = ["is_sm_ips_ports", "is_ftp_login", "swin", "dwin"]
 dataset_train[type_change] = dataset_train[type_change].astype(str)
